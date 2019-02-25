21bf3a757c66e0f6af7fc48b4237e52fd8e59892
Sebastian Sardina <ssardina@gmail.com>
2019-02-25 22:56:49 +1100

    Just in case added all paths to native library libjpl.so

--- set-SARL-env.bash
+++ set-SARL-env.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export LD_PRELOAD=$LD_PRELOAD:libswipl.so
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/swi-prolog/lib/x86_64-linux/
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/swi-prolog/lib/x86_64-linux/:/usr/lib/swi-prolog/lib/amd64/
 export SARL_VERSION=0.8.6
 
