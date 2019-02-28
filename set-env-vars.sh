9218cc6cc77f182b56199f0e28b0f8274d45041e
Sebastian Sardina <ssardina@gmail.com>
2019-02-28 17:57:31 +1100

    Improved set env script and bring back all tests

--- set-env-vars.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export SWI_HOME_DIR=/usr/local/swipl-git/lib/swipl/
-export LD_LIBRARY_PATH=$SWI_HOME_DIR/lib/x86_64-linux/:$SWI_HOME_DIR/lib/amd64/:$LD_LIBRARY_PATH
-export LD_PRELOAD=libswipl.so:$LD_PRELOAD
-
-export SARL_VERSION=0.8.6
-
